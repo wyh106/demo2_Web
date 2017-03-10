9475bd20d89ba144638f1d58403215822ae6216b
WYH106 <yuanhang.wang@mljr.com>
2017-03-10 15:18:10 +0800

    （28）SpringBoot启动时的Banner设置 （29）Spring boot 文件上传（多文件上传）

--- src/main/java/com/app/Demo2WebApplication.java
+++ src/main/java/com/app/Demo2WebApplication.java
@@ -6,12 +6,15 @@ import com.app.web.servlet.MyServlet1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @ServletComponentScan  // 这个就是扫描相应的Servlet包，结合注解注册serlvet使用
@@ -23,11 +26,26 @@ public class Demo2WebApplication extends SpringBootServletInitializer {
 		SpringApplication.run(Demo2WebApplication.class, args);
 	}
 
+	// 对文件做一些限制是有必要的，在App.java进行编码配置
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+		factory.setMaxFileSize("128KB"); //KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("256KB");
+		//Sets the directory location where files will be stored.
+		//factory.setLocation("路径地址");
+		return factory.createMultipartConfig();
+	}
+
+
+
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return super.configure(builder);
 	}
-
 	/**
 	 * 注册Servlet.不需要添加注解：@ServletComponentScan
 	 * @return
