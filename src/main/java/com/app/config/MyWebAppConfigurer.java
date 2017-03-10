d1728cbd7b6be5564bda3df98d1febf2cc483d15
WYH106 <yuanhang.wang@mljr.com>
2017-03-10 10:05:48 +0800

    （22）Spring Boot 拦截器HandlerInterceptor

---
+++ src/main/java/com/app/config/MyWebAppConfigurer.java
@@ -0,0 +1,25 @@
+package com.app.config;
+
+import com.app.web.intercepter.MyInterceptor1;
+import com.app.web.intercepter.MyInterceptor2;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by lenovo on 2017/3/10.
+ * 自定义Web配置
+ */
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 多个拦截器组成一个拦截器链
+        // addPathPatterns 用于添加拦截规则
+        // excludePathPatterns 用户排除拦截
+        registry.addInterceptor(new MyInterceptor1()).addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor2()).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+}
